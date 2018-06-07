4bb6bc3248cadfb4bc749f7e43de25d83554cd27
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-07 19:52:20 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,45 @@
 #Code your detention class here
+class Detention 
+
+def initialize
+  @activity = activity
+  @students = []
+end
+
+def add_student(name)
+ @students << @students.push(name)
+end
+
+def students
+  @students
+end
+
+def activity
+  @activity = "collective punishment"
+end
+
+def location
+  @location
+end
+
+def location= (location)
+  @location = location 
+end
+
+def time
+  @time 
+end 
+
+def time= (time)
+  @time = time 
+end
+
+def grumpy_teacher
+  @grumpy_teacher
+end
+
+def grumpy_teacher= (grumpy_teacher)
+  @grumpy_teacher = grumpy_teacher
+end
+
+end
